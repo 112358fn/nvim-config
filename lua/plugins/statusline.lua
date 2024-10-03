5a0691a569c0eee9f7e22986e597e04212e5f468
Alvaro Alonso <112358.fn@gmail.com>
2024-10-03 22:06:11 +0200

    Add buffer line

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,11 +1,24 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-      require('lualine').setup({
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
         options = {
-          theme = 'dracula'
-      }
-    })
-    end
+          theme = "dracula",
+        },
+      })
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+  },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      vim.opt.termguicolors = true
+      require("bufferline").setup({})
+    end,
+  },
 }
