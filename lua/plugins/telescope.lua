044f8089305bfa066b1ebc53f65c84b531710d5d
Alvaro Alonso <112358.fn@gmail.com>
2024-10-03 22:36:10 +0200

    Add telescope diagnostic

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fd', function() builtin.diagnostics({bufnr=0}) end, {desc= "Telescope diagnostics"})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
   end
   },
