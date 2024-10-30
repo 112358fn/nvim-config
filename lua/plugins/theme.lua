6c44c22bc86a441168a1ab38e7e12458a498dfc8
Alvaro Alonso <112358.fn@gmail.com>
2024-10-30 22:48:04 +0100

    Update format for theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,9 @@
 return {
-  "catppuccin/nvim",
-  name = "catpuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+	"catppuccin/nvim",
+	name = "catpuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
