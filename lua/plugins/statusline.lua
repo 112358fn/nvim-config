72bf6252af68a7ab4a500346361ea2b69f576c84
Alvaro Alonso <112358.fn@gmail.com>
2024-10-30 22:40:19 +0100

    Update format status line

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "dracula",
-        },
-      })
-    end,
-  },
-  {
-    "nvim-tree/nvim-web-devicons",
-  },
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      vim.opt.termguicolors = true
-      require("bufferline").setup({})
-    end,
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "dracula",
+				},
+			})
+		end,
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			vim.opt.termguicolors = true
+			require("bufferline").setup({})
+		end,
+	},
 }
