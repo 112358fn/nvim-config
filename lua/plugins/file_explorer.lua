7be4861d385eb81d8a54716319dcf7b49e296759
Alvaro Alonso <112358.fn@gmail.com>
2025-02-18 14:39:32 +0100

    Update neo-tree configuration

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "*",
+  lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -11,8 +12,11 @@ return {
     { "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
   },
   opts = {
+    close_if_last_window = true,
     filesystem = {
       hijack_netrw_behavior = "open_default",
+      use_libuv_file_watcher = false,
+      follow_current_file = { enabled = true, leave_dirs_open = true, },
       window = {
         mappings = {
           ["\\"] = "close_window",
