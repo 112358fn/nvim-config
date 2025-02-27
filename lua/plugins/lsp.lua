6336d60f2a30360fe6a627470accfdefc52f4c5b
Alvaro Alonso <112358.fn@gmail.com>
2025-02-27 17:44:12 +0100

    Clean up lsp pluggins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,70 +1,93 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"rust_analyzer",
-					"ruff",
-					"pyright",
-					"marksman",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.ruff.setup({ capabilities = capabilities })
-			lspconfig.pyright.setup({ capabilities = capabilities })
-			lspconfig.marksman.setup({ capabilities = capabilities, single_file_support = false })
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		end,
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.diagnostics.markdownlint,
-					null_ls.builtins.formatting.markdownlint,
-				},
-			})
-		end,
-	},
-	{
-		"nvimdev/guard.nvim",
-		dependencies = {
-			"nvimdev/guard-collection",
-		},
-		config = function()
-			vim.g.guard_config = {
-				fmt_on_save = true,
-				lsp_as_default_formatter = true,
-			}
-		end,
-	},
+  -- mason.nvim
+  -- Mason: Package Manager for LSP servers, DAP servers, linters and formatters
   {
-    "zk-org/zk-nvim",
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  -- mason-lspconfig.nvim
+  -- Mason extension for lspconfig as SOT
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    opts = {
+      automatic_installation = true,
+    },
+  },
+  -- mason-null-ls.nvim
+  -- Mason extension for none-ls as SOT
+  {
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls.nvim",
+    },
+    opts = {
+      automatic_installation = true,
+    },
+  },
+  -- nvim-lspconfig
+  -- neovim default configurations for LSP servers
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
     config = function()
-      require("zk").setup({})
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.ruff.setup({ capabilities = capabilities })
+      lspconfig.pyright.setup({ capabilities = capabilities })
+      lspconfig.marksman.setup({ capabilities = capabilities, single_file_support = false })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.yamlls.setup({ capabilities = capabilities })
+      lspconfig.bashls.setup({ capabilities = capabilities })
     end,
+  },
+  -- none-ls.nvim
+  -- Expose non-lsp tools as LSP sources
+  {
+    "nvimtools/none-ls.nvim",
+    main = "null-ls",
+    dependencies = {
+      "jay-babu/mason-null-ls.nvim",
+    },
+    opts = function()
+      local null_ls = require("null-ls")
+      return {
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.diagnostics.markdownlint,
+          null_ls.builtins.formatting.markdownlint,
+        },
+      }
+    end,
+  },
+  -- guard.nvim
+  -- Async formatting and linting
+  -- NOTE: only used for format on save
+  {
+    "nvimdev/guard.nvim",
+    init = function()
+      vim.g.guard_config = {
+        fmt_on_save = true, -- format on write to buffer
+        lsp_as_default_formatter = true,
+        save_on_fmt = true,
+        auto_lint = false,
+      }
+    end,
+  },
+  -- lspsaga.nvim
+  -- Improve nvim lsp experience
+  {
+    "nvimdev/lspsaga.nvim",
+    event = 'LspAttach',
+    config = true,
+    keys = {
+      { "<leader>ca", "<cmd>Lspsaga code_action<cr>", desc = "LSP code action" },
+      {"K", '<cmd>Lspsaga hover_doc<cr>', desc = "LSP hover"}
+    },
   },
 }
