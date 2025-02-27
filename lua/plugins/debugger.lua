cc5a7c9fada60075d3202cf049c2d5ce9417da2f
Alvaro Alonso <112358.fn@gmail.com>
2025-02-27 17:57:03 +0100

    Add debugers plugins

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,43 +1,47 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		-- dependecies = {
-		-- 	"rcarriga/nvim-dap-ui",
-		-- },
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
-			vim.keymap.set("n", "<leader>dt", function()
-				dap.toggle_breakpoint()
-			end)
-			vim.keymap.set("n", "<leader>dc", function()
-				dap.continue()
-			end)
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
-		end,
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-		config = function()
-			require("dapui").setup()
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap-python",
-		config = function()
-			require("dap-python").setup("python")
-		end,
-	},
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      vim.keymap.set("n", "<leader>dt", function()
+        dap.toggle_breakpoint()
+      end)
+      vim.keymap.set("n", "<leader>dc", function()
+        dap.continue()
+      end)
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+  { "nvim-neotest/nvim-nio" },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
+    },
+    config = true,
+    main = "dapui",
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      require("dap-python").setup("python")
+    end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    config = true,
+  },
 }
