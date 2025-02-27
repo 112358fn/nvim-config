72bc4fa4246beb003c512d3dc987331742c1be89
Alvaro Alonso <112358.fn@gmail.com>
2025-02-27 17:50:56 +0100

    Merge theme related pluggins

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,25 @@
 return {
-	"catppuccin/nvim",
-	name = "catpuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-	end,
+  {
+    "catppuccin/nvim",
+    name = "catpuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-latte")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      options = {
+        theme = "catppuccin",
+      },
+    },
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+  },
 }
