bc6dd94286afb2916caa654999b338295f4fc473
Alvaro Alonso <112358.fn@gmail.com>
2025-03-12 19:37:25 +0100

    Improve ui

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,7 +5,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("catppuccin-latte")
+      vim.cmd.colorscheme("catppuccin-mocha")
     end,
   },
   {
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,11 @@
 return {
   { "MunifTanjim/nui.nvim" },
-  { "rcarriga/nvim-notify" },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      max_width = 60,
+    },
+  },
   {
     "folke/noice.nvim",
     event = "VeryLazy",
@@ -9,6 +14,9 @@ return {
       "rcarriga/nvim-notify",
     },
     opts = {
+      cmdline = {
+        view = "cmdline",
+      },
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
