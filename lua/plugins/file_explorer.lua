aa218ede018f60ec85ce0b956bc4cbac09880f27
Alvaro Alonso <112358.fn@gmail.com>
2025-03-12 21:39:45 +0100

    Format file_explorer pluging

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -1,25 +1,32 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  cmd = "Neotree",
-  keys = {
-    { "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
-  },
-  opts = {
-    close_if_last_window = true,
-    filesystem = {
-      hijack_netrw_behavior = "open_default",
-      use_libuv_file_watcher = false,
-      follow_current_file = { enabled = true, leave_dirs_open = true, },
-      window = {
-        mappings = {
-          ["\\"] = "close_window",
+  { "nvim-lua/plenary.nvim" },
+  { "s1n7ax/nvim-window-picker" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    keys = {
+      { "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+    },
+    opts = {
+      close_if_last_window = true,
+      filesystem = {
+        hijack_netrw_behavior = "open_default",
+        use_libuv_file_watcher = false,
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+        window = {
+          mappings = {
+            ["\\"] = "close_window",
+          },
         },
       },
     },
