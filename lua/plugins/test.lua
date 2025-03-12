a3060c24207d6085c9212295af0690c359cf47e6
Alvaro Alonso <112358.fn@gmail.com>
2025-03-12 19:34:31 +0100

    Add pluggins for test

---
+++ lua/plugins/test.lua
@@ -0,0 +1,42 @@
+return {
+  { "nvim-neotest/nvim-nio" },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python"),
+          require("neotest-rust")({
+            args = { "--no-capture" },
+            dap_adapter = "codelldb",
+          }),
+        },
+      })
+    end,
+    keys = {
+      {
+        "<leader>dt",
+        function()
+          require("neotest").run.run({ strategy = "dap" })
+        end,
+        desc = "[d]ebug [t]est",
+      },
+      {
+        "<leader>tt",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "[t]oggle [t]est summary",
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest-python",
+  },
+  {
+    "rouge8/neotest-rust",
+  },
+}
