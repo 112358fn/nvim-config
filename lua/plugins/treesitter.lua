945726d375f45b84d2d43f47982009eff127902b
Alvaro Alonso <112358.fn@gmail.com>
2025-04-18 19:28:09 +0200

    add: folding using treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,50 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "python", "rust" },
-      highlight = { enable = true },
-      indent = { enable = true },
+        ensure_installed = { "lua", "python", "rust", "go" },
+        highlight = { enable = true },
+        indent = { enable = true },
     })
-  end,
+    end,
+  },
+  -- nvim-ufo
+  -- Ultra Fold in neovim
+  { "kevinhwang91/promise-async" },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+    },
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { "treesitter", "indent" }
+      end,
+    },
+    keys = {
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+        desc = "Open all folds",
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+        desc = "Close all folds",
+      },
+    },
+    init = function()
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+  },
 }
