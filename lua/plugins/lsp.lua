a84d7f2cd94d3e03016500f2cbc9af27642cf74a
Alvaro Alonso <112358.fn@gmail.com>
2025-05-28 16:36:02 +0200

    Replace nvim-cmp with blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,9 +16,10 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
+      "saghen/blink.cmp",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.ruff.setup({ capabilities = capabilities })
