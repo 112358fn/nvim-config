130df645dda5e074987245ba6fa4a547a8069601
Alvaro Alonso <112358.fn@gmail.com>
2026-02-16 18:12:59 +0100

    shell: update nixpkgs 25.11

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import (fetchTarball "https://api.github.com/repos/NixOS/nixpkgs/tarball/25.05") { };
+  pkgs = import (fetchTarball "https://api.github.com/repos/NixOS/nixpkgs/tarball/25.11") { };
 in
 pkgs.mkShellNoCC {
   packages = with pkgs; [
