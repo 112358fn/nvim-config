254aec830861e1efceb381163195273585e07318
Alvaro Alonso Bivou <112358.fn@gmail.com>
2026-06-30 19:37:13 +0200

    plugins/theme: rm deprecated option

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -13,11 +13,7 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    opts = {
-      options = {
-        theme = "catppuccin",
-      },
-    },
+    opts = {},
   },
   {
     "nvim-tree/nvim-web-devicons",
